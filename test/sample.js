6a4493ff4b2f76ee2d6b2fbe11b71ed9ae7a0f43
Matzo <ksk.matsuo@gmail.com>
2013-01-14 22:52:03 +0900

    update sample

--- test/sample.js
+++ test/sample.js
@@ -1,33 +1,34 @@
 {
-    "announce" : [
+    "mapObject1" : [
         {
             "date" : "2013/01/02",
             "title" : "A Happy New Year",
             "url" : "https://www.google.co.jp/search?q=A+Happy+New+Year",
-            "openIn" : "window",
-            "target" : ["android","windows","ios","web"]
+            "type" : "select1",
+            "attributes" : ["attr1","attr3"]
         },
         {
             "date" : "2012/12/31",
             "title" : "New Year's Eve",
             "url" : "https://www.google.co.jp/search?q=New+Year's+Eve",
-            "openIn" : "iframe",
-            "target" : ["web"]
+            "type" : "select2",
+            "attributes" : ["attr2","attr4"]
         }
     ],
-    "maintenance" : [
+    "mapObject2" : [
         {
             "date" : "2012/12/18",
             "title" : "Service Maintenance 1",
             "url" : "https://www.google.co.jp/search?q=Service+Maintenance",
-            "target" : ["android","windows","ios"]
+            "type" : "",
+            "attributes" : ["attr4"]
        },
        {
             "date" : "2012/12/26",
             "title" : "Service Maintenance 2",
             "url" : "https://www.google.co.jp/search?q=Service+Maintenance+2",
-            "openIn" : "window",
-            "target" : ["android","windows","ios","web"]
+            "type" : "select3",
+            "attributes" : ["attr1"]
         }
     ]
 }
